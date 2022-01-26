42bf8afef8ffc0b6c93f1b0e868fefdd7c4e5501
Gill Chin <gill74123@gmail.com>
2022-01-26 16:42:23 +0800

    表單驗證 登出功能

--- js/products.js
+++ js/products.js
@@ -4,6 +4,7 @@ import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue
 // 宣告 Modal 變數(因為要讓 methods 吃到)
 let productModal = "";
 let delProductModal = "";
+let logoutModal = "";
 
 const app = createApp({
     data() {
@@ -15,6 +16,7 @@ const app = createApp({
             tempProduct: {
                 imagesUrl: []
             },
+            remindMessage: "",
         }
     },
     methods: {
@@ -24,7 +26,7 @@ const app = createApp({
             axios.post(url)
                 .then((res) => {
                     console.log(res);
-                    
+
                     // 執行 取得產品資料
                     this.getData();
                 })
@@ -51,7 +53,10 @@ const app = createApp({
                 })
         },
         // 開啟產品 modal (新增 & 編輯公用)
-        openProductModal(modalStatus, item) {            
+        openProductModal(modalStatus, item) {
+            // 驗證表單用 - 每開一次新增編輯的 modal 就清空 remindMessage
+            this.remindMessage = "";
+
             // 判斷開啟的 modal 是新增還是編輯
             if (modalStatus === 'new') {
                 // 新增 - 清空 tempProduct 物件
@@ -66,7 +71,7 @@ const app = createApp({
                 productModal.show();
             } else if (modalStatus === 'edit') {
                 // 編輯 - 將原有的產品資料展開
-                this.tempProduct = {...item};
+                this.tempProduct = { ...item };
 
                 // 新增頁面將 isNew 改成 false
                 this.isNew = false;
@@ -75,10 +80,13 @@ const app = createApp({
                 productModal.show();
             } else if (modalStatus === 'delete') {
                 // 刪除 - 將原有的產品資料展開
-                this.tempProduct = {...item};
+                this.tempProduct = { ...item };
 
                 // 開啟 modal
                 delProductModal.show();
+            } else if (modalStatus === 'logout') {
+                // 開啟 modal
+                logoutModal.show();
             }
         },
         // 新增 & 編輯產品 (新增 & 編輯共用同個方法)
@@ -92,32 +100,65 @@ const app = createApp({
                 httpMethod = "put";
             }
 
-            axios[httpMethod](url, {data: this.tempProduct})
-            .then((res) => {
-                console.log(res);
+            // if (!this.tempProduct.title) {
+            //     console.log(1);
+            //     // this.validation();
 
-                // 關閉 modal
-                productModal.hide();
+            // } else {
+            axios[httpMethod](url, { data: this.tempProduct })
+                .then((res) => {
+                    console.log(res);
 
-                // 執行 取得產品資料
-                this.getData();
-            })
-            .catch((err) => {
-                console.dir(err);
-            })
+                    // 關閉 modal
+                    productModal.hide();
+
+                    // 執行 取得產品資料
+                    this.getData();
+                })
+                .catch((err) => {
+                    console.dir(err);
+                })
+            // }
+
+        },
+        // 驗證表單
+        validation(id) {
+            if (!this.tempProduct.title || !this.tempProduct.category || !this.tempProduct.unit || !this.tempProduct.origin_price || !this.tempProduct.price) {
+                this.remindMessage = "必填項目！"
+            } else {
+                this.remindMessage = "";
+                this.updateProduct(id);
+            }
         },
         // 刪除產品
         deleteProduct(id) {
             const url = `${this.baseUrl}/api/${this.apiPath}/admin/product/${id}`;
             axios.delete(url)
+                .then((res) => {
+                    console.log(res);
+
+                    // 關閉 modal
+                    delProductModal.hide();
+
+                    // 執行 取得產品資料
+                    this.getData();
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        },
+        // 登出
+        logout() {
+            const url = `${this.baseUrl}/logout`;
+            axios.post(url)
             .then((res) => {
                 console.log(res);
 
                 // 關閉 modal
-                delProductModal.hide();
-                
-                // 執行 取得產品資料
-                this.getData();
+                logoutModal.hide();
+
+                // 頁面跳轉
+                // window.location = "./login.html";
             })
             .catch((err) => {
                 console.log(err);
@@ -134,9 +175,11 @@ const app = createApp({
 
         // 使用 new 建立 bootstrap modal，拿到實體 DOM 並賦予到變數上
         // 新增 & 編輯的 modal
-        productModal = new bootstrap.Modal(document.querySelector("#productModal"), {keyboard: false});
+        productModal = new bootstrap.Modal(document.querySelector("#productModal"), { keyboard: false });
         // 刪除的 modal
-        delProductModal = new bootstrap.Modal(document.querySelector("#delProductModal"), {keyboard: false});
+        delProductModal = new bootstrap.Modal(document.querySelector("#delProductModal"), { keyboard: false });
+        // 登出的 modal
+        logoutModal = new bootstrap.Modal(document.querySelector("#logoutModal"), { keyboard: false });
     },
 })
 
